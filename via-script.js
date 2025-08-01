410072c794afe080adcf706102b94b3f4aeefac0
vicoho <44053831+vicoho@users.noreply.github.com>
2025-08-01 14:04:25 +0800

    Add files via upload

--- via-script.js
+++ via-script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         挂刀页面美化
 // @namespace    https://github.com/vicoho/Steam-Market-Calculator
-// @version      0.38
+// @version      0.4
 // @description  优化smis.club挂刀页面的显示效果
 // @author       vicoho
 // @run-at       document-end
